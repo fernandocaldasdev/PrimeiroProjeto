5cb11a8041a64cc037340f5af8b393ad5000820a
Fernando Costa <contact.fernandocosta@gmail.com>
2023-09-06 14:20:05 -0300

    Primeiro projeto

---
+++ app/src/main/java/com/fernandocosta/primeiroprojeto/MainActivity.kt
@@ -0,0 +1,21 @@
+    package com.fernandocosta.primeiroprojeto
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+import java.util.Random
+
+    class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun sortear(view: View){
+        val textoResultado = findViewById<TextView>(R.id.text_resultado)
+        val numero = Random().nextInt(10)
+        textoResultado.setText("número gerado $numero")
+
+    }
+}
